ccb36d0dbecac95fc654942af36e4f5295c51eee
JeremyAnderson8 <janderson@chapman.edu>
2020-05-06 21:55:22 +0000

    Faculty Class, Finish List of Advisee IDs

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,11 @@
 #include "Student.h"
+#include "Faculty.h"
 
 int main(int argc, char** argv){
 
   Student *jeremy = new Student(2314466, "Jeremy Anderson", "Sophomore", "Data Analytics", 3.4, 111111);
+  Faculty *rene = new Faculty(123456, "Rene German", "Lecturer", "Computer Science");
 
-
+  cout << rene->getName() << endl;
 
 }
